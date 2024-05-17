c0f9d3b48b1d295c5ca9a256a9bd199568203e3a
Valeriya Polyanskaya <sp.ether@yandex.ru>
2024-05-17 03:01:45 +0300

    adds header navigation

---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,37 @@
+import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import './app-header.css';
+
+const AppHeader = () => {
+  return (
+    <header className='app-header p-4'>
+      <div className='wrapper'>
+        <nav className='navigation'>
+          <ul className='nav-list text text_type_main-default'>
+            <li className='nav-item'>
+              <a className='nav-link pr-5 pl-5 pt-4 pb-4' href="">
+                <BurgerIcon type="primary" />
+                <span className='pl-2'>Конструктор</span>
+                </a>
+              </li>
+            <li className='nav-item'>
+              <a className='nav-link pr-5 pl-5 pt-4 pb-4' href="">
+              <ListIcon type="primary" />
+              <span className='pl-2'>Лента заказов</span>
+              </a>
+              </li>
+            <li className='nav-item'>
+              <a className='nav-link pr-5 pl-5 pt-4 pb-4' href="">
+              <ProfileIcon type="primary" />
+              <span className='pl-2'>Личный кабинет</span>
+              </a>
+              </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default AppHeader;
---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppHeader from '../app-header/app-header';
+import '@ya.praktikum/react-developer-burger-ui-components';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
